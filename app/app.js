c41b15b601e205c3f39d211b2c89e10c49709136
Raviraj Singh <raviraj.s@consultadd.com>
2020-01-27 17:58:36 -0500

    Project commit

---
+++ app/app.js
@@ -0,0 +1,49 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ui.router'])
+        .config(config)
+        .run(run);
+
+    function config($stateProvider, $urlRouterProvider) {
+        // default route
+        $urlRouterProvider.otherwise("/");
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'home/index.html',
+                controller: 'Home.IndexController',
+                controllerAs: 'vm',
+                data: { activeTab: 'home' }
+            })
+            .state('account', {
+                url: '/account',
+                templateUrl: 'account/index.html',
+                controller: 'Account.IndexController',
+                controllerAs: 'vm',
+                data: { activeTab: 'account' }
+            });
+    }
+
+    function run($http, $rootScope, $window) {
+       
+        $http.defaults.headers.common['Authorization'] = 'Bearer ' + $window.jwtToken;
+
+        
+        $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            $rootScope.activeTab = toState.data.activeTab;
+        });
+    }
+
+  
+    $(function () {
+      
+        $.get('/app/token', function (token) {
+            window.jwtToken = token;
+
+            angular.bootstrap(document, ['app']);
+        });
+    });
+})();
